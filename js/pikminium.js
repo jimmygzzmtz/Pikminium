9a94f42c95c48848d66786123dd33c1cc252c49e
Jaime González <jimmygzzmtz@gmail.com>
2020-10-26 22:01:25 -0600

    Remove VRButton Reference

--- js/pikminium.js
+++ js/pikminium.js
@@ -1,5 +1,3 @@
-document.body.appendChild( VRButton.createButton( renderer ) );
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
